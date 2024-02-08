09c13b4e20f6c4fbe69bf2a1586f364445389f55
Enoch Taylor <tay121222@gmail.com>
2024-02-08 11:00:12 +0000

    added parameter and output type

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """function called filter_datum"""
+from typing import List
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """returns the log message obfuscated"""
     fields_regex = '|'.join(fields)
     return re.sub(
