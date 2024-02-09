476643f23e9078c972194851272dc1a133dd2440
Enoch Taylor <tay121222@gmail.com>
2024-02-09 09:08:51 +0000

    added is_valid function that expects 2 arguments and returns a boolean

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,7 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """is_valid function that expects 2 arguments and returns a boolean"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
