569c254c641f466618880718045c4d9afc487443
Enoch Taylor <tay121222@gmail.com>
2024-02-16 22:13:54 +0000

    method def extract_user_credentials returns the user email and password from the Base64 decoded value

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -40,3 +40,19 @@ class BasicAuth(Auth):
             return decoded_string
         except base64.binascii.Error:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str
+            ) -> (str, str):
+        """returns the user email and password from the
+        Base64 decoded value"""
+        if decoded_base64_authorization_header is None or not isinstance(
+                decoded_base64_authorization_header, str
+                ):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        uemail, upass = decoded_base64_authorization_header.split(':', 1)
+        return uemail, upass
