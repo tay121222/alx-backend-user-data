9f678eb8f8cdf22f73bdf9eed1236ebe38e564ee
Enoch Taylor <tay121222@gmail.com>
2024-02-16 22:04:51 +0000

    method decode_base64_authorization_header that returns the decoded value of a Base64 string base64_authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """class BasicAuth"""
 from .auth import Auth
-
+import base64
 
 class BasicAuth(Auth):
     """inherits from Auth"""
@@ -20,3 +20,22 @@ class BasicAuth(Auth):
 
         base64_part = authorization_header.split(' ')[1]
         return base64_part
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str
+            ) -> str:
+        """returns the decoded value of a Base64 string
+        base64_authorization_header"""
+        if base64_authorization_header is None or not isinstance(
+                base64_authorization_header, str
+                ):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(
+                    base64_authorization_header
+                    )
+            decoded_string = decoded_bytes.decode('utf-8')
+            return decoded_string
+        except base64.binascii.Error:
+            return None
