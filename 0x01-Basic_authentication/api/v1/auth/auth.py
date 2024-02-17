a00ecadb23f5a75c54404d4427b13e19f53d36cf
Enoch Taylor <tay121222@gmail.com>
2024-02-17 06:26:23 +0000

    Improve def require_auth(self, path, excluded_paths) by allowing * at the end of excluded paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -16,11 +16,13 @@ class Auth:
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
 
-        if not path.endswith("/"):
-            path += "/"
-
-        if path in excluded_paths:
-            return False
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('*') and path.startswith(
+                    excluded_path[:-1]
+                    ):
+                return False
+            elif path == excluded_path:
+                return False
 
         return True
 
