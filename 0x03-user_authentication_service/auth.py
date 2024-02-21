cc6382a301ea7c3f347f1ee73177c96cf17e39a7
Enoch Taylor <tay121222@gmail.com>
2024-02-21 22:01:51 +0000

    Auth.register_user method in Auth class that takes mandatory email and password string arguments and return a User object.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python3
 """contains _hash_password method"""
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """takes in a password string arguments and returns bytes"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """Initialization"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """register new user with email and pass"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
