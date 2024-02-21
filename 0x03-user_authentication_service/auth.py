94a8c74e3f108848a0169df7c2f07bc54ab902e5
Enoch Taylor <tay121222@gmail.com>
2024-02-21 22:38:31 +0000

    _generate_uuid function in the auth module that should return a string representation of a new UUID. Use the uuid module.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -39,3 +40,7 @@ class Auth:
         except NoResultFound:
             return False
         return False
+
+    def _generate_uuid() -> str:
+        """return a string representation of a new UUID"""
+        return str(uuid.uuid4())
