d556ca8060bc583820aeffdbec40612cafebc531
Enoch Taylor <tay121222@gmail.com>
2024-02-21 22:47:59 +0000

    Auth.create_session method. It takes an email string argument and returns the session ID as a string.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -45,3 +45,13 @@ class Auth:
         except NoResultFound:
             return False
         return False
+
+    def create_session(self, email: str) -> str:
+        """Create a session for user"""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = str(uuid.uuid4())
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
