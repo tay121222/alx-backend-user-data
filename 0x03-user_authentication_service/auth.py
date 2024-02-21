52a021a32331e025b17a3528fb17ca2b50fac37d
Enoch Taylor <tay121222@gmail.com>
2024-02-21 22:31:53 +0000

    Auth.valid_login method that expects email and password required arguments and return a boolean.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -28,3 +28,14 @@ class Auth:
             hashed_password = _hash_password(password)
             user = self._db.add_user(email, hashed_password)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Authenticate user logins credential"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode(
+                'utf-8'
+                ), user.hashed_password)
+        except NoResultFound:
+            return False
+        return False
