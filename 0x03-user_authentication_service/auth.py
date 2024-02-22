dfdab7bf4c8618202d01fe009a2b62f125ca37c9
Enoch Taylor <tay121222@gmail.com>
2024-02-22 23:01:53 +0000

    Auth.destroy_session. The method takes a single user_id integer argument and returns None.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -66,3 +66,7 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int):
+        """updates the corresponding user’s session ID to None"""
+        self._db.update_user(user_id, session_id=None)
