9f634ee86a80326e61b939d8ddf7379d52642971
Enoch Taylor <tay121222@gmail.com>
2024-02-22 22:51:22 +0000

    impelemted login function to respond to the POST /sessions route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Basic flask app"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 app = Flask(__name__)
@@ -24,6 +24,27 @@ def register_user():
                 ), 200
     except ValueError as e:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """login function to respond to the POST /sessions route
+    return a JSON payload of the form"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        if session_id is None:
+            abort(401)
+
+        response = make_response(jsonify(
+            {"email": email, "message": "logged in"}
+            ), 200)
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
