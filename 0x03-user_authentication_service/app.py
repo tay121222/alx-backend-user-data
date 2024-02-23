de5e53ed20c488fb5a259cafda45c1be250f8e87
Enoch Taylor <tay121222@gmail.com>
2024-02-23 04:45:10 +0000

    logout function to respond to the DELETE /sessions route - added import flask redirect

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Basic flask app"""
-from flask import Flask, jsonify, request, abort, make_response
+from flask import Flask, jsonify, request, abort, make_response, redirect
 from auth import Auth
 
 app = Flask(__name__)
