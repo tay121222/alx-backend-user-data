e67a258d27f56f9a3565fa26dbbb0c5d999442ae
Enoch Taylor <tay121222@gmail.com>
2024-02-23 04:59:11 +0000

    implemented a profile function to respond to the GET /profile route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -60,5 +60,16 @@ def logout() -> str:
         return abort(403)
 
 
+@app.route('/profile', methods=['GET'])
+def profile() -> str:
+    """function to respond to the GET /profile route"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is not None:
+        return jsonify({"email": user.email}), 200
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
